3cf315860502a9c7946fb70ee9434073343480d3
sam <zerzamo@gmail.com>
2021-04-16 13:23:11 +0400

    Initial Commit

---
+++ appligit/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  appligit
+//
+//  Created by SamuelS on 16/04/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+

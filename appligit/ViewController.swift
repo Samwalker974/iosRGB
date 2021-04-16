3bcb0a13f132e1607b1996befc5c707c9e41631c
sam <zerzamo@gmail.com>
2021-04-16 14:23:07 +0400

    Slide Fonctionnelle

--- appligit/ViewController.swift
+++ appligit/ViewController.swift
@@ -8,12 +8,26 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var redAff: UILabel!
+    @IBOutlet weak var greenAff: UILabel!
+    @IBOutlet weak var blueAff: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func redSlide(_ sender: UISlider) {
+        redAff.text = "Rouge : \(Int(sender.value))%"
+    }
+    @IBAction func greenSlide(_ sender: UISlider) {
+        greenAff.text = "Vert : \(Int(sender.value))%"
+    }
+    @IBAction func blueSlide(_ sender: UISlider) {
+        blueAff.text = "Bleu : \(Int(sender.value))%"
+    }
+    
+    
 }
 
